0a3693197fccdf51f76749a0cf627858154a376f
sayantan1821 <ksayantan1823@gmail.com>
2021-12-13 21:31:35 +0530

    lc q1446

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,6 +2,9 @@ package Main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(q678.nthMagicalNumber(1, 2, 3));
+//        System.out.println(q678.nthMagicalNumber(1, 2, 3));
+        System.out.println(q1446.maxPower("leetcode"));
     }
+
+
 }
---
+++ src/Main/q1446.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class q1446 {
+    public static int maxPower(String s) {
+        if(s.length() == 1) return 1;
+        int max = 0;
+        int count = 1;
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) != s.charAt(i-1)) {
+                if(count > max) max = count;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        if(count > max) max = count;
+        return max;
+    }
+}
