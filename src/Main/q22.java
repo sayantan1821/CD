5f41c9c1ce4fd1b0c9f1101b5ad342ba6441c514
sayantan1821 <ksayantan1823@gmail.com>
2021-12-15 02:21:37 +0530

    update lc q22

--- src/Main/q22.java
+++ src/Main/q22.java
@@ -5,39 +5,53 @@ import java.util.List;
 
 public class q22 {
     public static void main() {
-        List<String> str = new ArrayList<>();
+        List<String> str;
         str =  generateParenthesis(3);
-        for(int i=0; i < str.size(); i++){
-            System.out.println( str.get(i) );
+        for (String s : str) {
+            System.out.println(s);
         }
     }
 
-    public void main(String[] args) {
+//    public void main(String[] args) {
+//
+//    }
+//    public static List<String> generateParenthesis(int n) {
+//        // TC - O(pow(2, 2n)), SC - O(n)
+//        List<String> res = new ArrayList<>();
+//        dfs(res, new StringBuilder(), n, n, n);
+//        return res;
+//    }
+//
+//    public static void dfs(List<String> res, StringBuilder sb, int left, int right, int n) {
+//        if(left == 0 && right == 0) {
+//            res.add(sb.toString());
+//            return;
+//        }
+//
+//        if(left > 0) {
+//            sb.append("(");
+//            dfs(res, sb, left - 1, right, n);
+//            sb.deleteCharAt(sb.length() - 1);
+//        }
+//
+//        if(right > left) {
+//            sb.append(")");
+//            dfs(res, sb, left, right - 1, n);
+//            sb.deleteCharAt(sb.length() - 1);
+//        }
+//    }
 
-    }
     public static List<String> generateParenthesis(int n) {
-        // TC - O(pow(2, 2n)), SC - O(n)
-        List<String> res = new ArrayList<>();
-        dfs(res, new StringBuilder(), n, n, n);
-        return res;
+        List<String> ans = new ArrayList<>();
+        findParenthesis(ans, "(", 1, 0, n);
+        return ans;
     }
-
-    public static void dfs(List<String> res, StringBuilder sb, int left, int right, int n) {
-        if(left == 0 && right == 0) {
-            res.add(sb.toString());
+    public static void findParenthesis(List<String> ans, String res, int o, int c, int n) {
+        if(res.length() == 2 * n) {
+            ans.add(res);
             return;
         }
-
-        if(left > 0) {
-            sb.append("(");
-            dfs(res, sb, left - 1, right, n);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-
-        if(right > left) {
-            sb.append(")");
-            dfs(res, sb, left, right - 1, n);
-            sb.deleteCharAt(sb.length() - 1);
-        }
+        if(o < n) findParenthesis(ans, res + "(", o + 1, c, n);
+        if(c < o) findParenthesis(ans, res + ")", o, c + 1, n);
     }
 }
